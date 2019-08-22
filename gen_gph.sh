dd0d184d02b522b0a331d575ce4a9e3a026cd715
Andrew Fuller <qartis@gmail.com>
2019-08-22 15:57:15 -0700

    git-annex in qartis@trench:~/web/posts

--- gen_gph.sh
+++ gen_gph.sh
@@ -1,15 +1,1 @@
-#!/bin/sh
-
-#[0|post.txt|post.txt|server|port]
-#[1|photos|photos/|server|port]
-
-ls */post.txt | while read f; do
-	echo -n "[1|"
-	cat $f | awk '/^date: / {printf $2}'
-	echo -n " "
-	cat $f | awk -F'"' '/^title: / {printf $2}'
-	echo -n "|"
-	echo -n $(dirname $f)
-	echo -n "|server|port]"
-	echo
-done
+/annex/objects/SHA256E-s375--e8b3b86e2dd90e4c66bc9abc53a47f35a9436ac2d60fb9dc68196e07018d95fe.sh
