c01e7250d3ad75bddbd1d3799fd7628dccd21b56
Andrew Fuller <qartis@gmail.com>
2019-10-02 06:50:32 +0000

    git-annex in qartis@swich:~/posts

--- files/gen_html.sh
+++ files/gen_html.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 echo '<link rel="alternate" type="application/atom+xml" href="/index.xml">'
-echo '<style>body{font-size:1.2em}</style>'
+echo '<style>
+body{font-size:1.2em}
+li {
+text-overflow: ellipsis;
+white-space: nowrap;
+overflow: hidden;
+}
+</style>'
 
 ./files/posts.sh | awk -F'\x1f' '{printf "<li>%s <a href='%s'>%s</a>\n",$1,$2,$3}'
 
